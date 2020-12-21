21c4e36b7d524961321d1de2bb56d3c07b76c7e3
tanzil786 <tanzilidrisi68@gmail.com>
2020-12-21 15:01:29 +0530

    Create index.js

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Spacer } from "./Spacer";
+export { default as NoteList } from "./NoteList";
+export { default as NoteForm } from "./NoteForm";
+export { default as NoteModal } from "./NoteModal";
+export { default as EmptyState } from "./EmptyState";
